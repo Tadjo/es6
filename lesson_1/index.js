50d88201e497255f1763cc9f5f62858d517df204
Konstantin Bulgakov <nor.wanderer@gmail.com>
2019-02-22 12:34:32 +0300

    lesson 1

--- lesson_1/index.js
+++ lesson_1/index.js
@@ -1,60 +1,87 @@
 'use strict'
-/* Блочная область видимости, помимо глобальной функциональной обдасти видимости */
-// let a = 'outer';
-// {
-//     let a = 'inner';
-//     log(`Inside: ${a}`);
+/* Syntax + block scoping intro */
+// let x = 1;
+// const y = 2;
+////const y; Error !!!
+// console.log(x, y);
+
+// function withBlockScope() {
+//     let tst = 1;
+//     if (true) {
+//         let tst = 2;
+//         console.log('Inside:', tst);
+//     }
+//     console.log('Out:', tst);
 // }
-// log(`Out: ${a}`);
+// withBlockScope();
+// {
+//     let tst = 1;
+//     if (true) {
+//         let tst = 2;
+//         console.log('Inside:', tst);
+//     }
+//     console.log('Out:', tst);
+// }
+// console.log(tst); // Cant access tst variable in outer scope
 
 /* Где использовать */
 // let callbacks = [];
 // for (let i = 1; i <= 10; i++) {
 //     callbacks.push(function() {
-//         log(i);
+//         console.log(i);
 //     });
 // }
 // callbacks.forEach(function(cb) { cb()});
 
-/* hoisting */
-// console.log(hoist);
+
+/* hoisting + TDZ */
+// if (1) {
+//     console.log('Var:', hoistVar);
+//     console.log('Let:', hoist);
+//     var hoistVar;
+//     const hoist;
+// }
+
+
+/* Immutable */
+// const unchangable = {nested: {value: 2}};
+// Object.defineProperty(unchangable, 'value', { /* writable: false, */  get() { return 10}, set(value) { throw new Error('immutable')}});
+// Object.freeze(unchangable);
+// Object.getOwnPropertyNames(unchangable).forEach(function(key) {
+//     if (typeof unchangable[key] === "object" && unchangable[key] !== null) {
+//         Object.freeze(unchangable[key]);
+//     }
+// })
 // try {
-//     typeof hoist;
+//     unchangable.value = 1;
+//     unchangable.nested.value = 3;
 // } catch (error) {
 //     console.log(error);
+// } finally {
+//     console.log(unchangable);
 // }
-// let hoist;
 
-// 'use strict'
-/* const */
-const unchangable = {nested: {value: 2}};
-// Object.defineProperty(unchangable, 'value', { /* writable: false, */  get() { return 10}});
-Object.freeze(unchangable);
-Object.getOwnPropertyNames(unchangable).forEach(function(key) {
-    if (typeof unchangable[key] === "object" && unchangable[key] !== null) {
-        Object.freeze(unchangable[key]);
-    }
-})
-try {
-    unchangable.value = 1;
-    unchangable.nested.value = 3;
-} catch (error) {
-    console.log(error);
-} finally {
-    console.log(unchangable);
-}
+/* blocked scope funcitons */
+// if (1) {
+//     inside();
+//     function inside() {
+//         return true;
+//     }
+// }
+// console.log(inside);
 
-
+// !! madke in console or disable webpack
 /* const and let не создают глобальных переменных */
 // let globvar = 1;
 // let globfn = function() {};
 // console.log(window.globvar, window.globfn);
 
-/* blocked scope funcitons */
-// if (true) {
-//     foo();
-//     function foo() {
-//         return true;
-//     }
-// }
-// console.log(foo);
+/* function declaration не создают глобальных переменных  */
+// function glob() {};
+// console.log(glob);
+// console.log(window.glob);
+
+/* class declaration не создаen глобальных переменных  */
+// class notGlobal {};
+// console.log(notGlobal);
+// console.log(window.notGlobal);
