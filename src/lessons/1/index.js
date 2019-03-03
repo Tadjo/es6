b3242c7fb38c498f2ab4d1b0a32b3e31c2975949
Tadjo <nor.wanderer@gmail.com>
2019-03-03 20:57:09 +0300

    lesson 2 update

--- src/lessons/1/index.js
+++ src/lessons/1/index.js
@@ -1,6 +1,7 @@
 'use strict'
+
 // function withBlockScope() {
-//     let tst = 1;
+//     var tst = 1;
 //     if (true) {
 //         let tst = 2;
 //         console.log('Inside:', tst);
@@ -8,6 +9,7 @@
 //     console.log('Out:', tst);
 // }
 // withBlockScope();
+
 // {
 //     let tst = 1;
 //     if (true) {
@@ -20,9 +22,23 @@
 
 /* Где использовать */
 // let callbacks = [];
-// for (let i = 1; i <= 10; i++) {
-//     callbacks.push(function() {
-//         console.log(i);
+// for (var i = 1; i <= 5; i++) {
+//     // Solution 1 
+//     (function() {
+//         var k = i;
+//         callbacks.push(function() {
+//             console.log(k**2);
+//         });
+//     })();
+//     // Solution 2 
+//     callbacks.push(function(i) {
+//         console.log(i**2);
+//     }.bind(null, i));
+// }
+// es6 way
+// for (let i = 1; i <= 5; i++) {
+//     callbacks.push(function () {
+//         console.log(i ** 2);
 //     });
 // }
 // callbacks.forEach(function(cb) { cb()});
@@ -33,26 +49,44 @@
 //     console.log('Var:', hoistVar);
 //     console.log('Let:', hoist);
 //     var hoistVar;
-//     const hoist;
+//     let hoist;
+// }
+
+// TDZ is time zone, not code zone
+// if (1) {
+//     function tryGetLet() {
+//         console.log(tdz);
+//     }
+//     // tryGetLet() // Error TDZ
+//     let tdz;
+//     tryGetLet();
 // }
 
 
 /* Immutable */
 // const unchangable = {nested: {value: 2}};
 // Object.defineProperty(unchangable, 'value', { /* writable: false, */  get() { return 10}, set(value) { throw new Error('immutable')}});
-// Object.freeze(unchangable);
-// Object.getOwnPropertyNames(unchangable).forEach(function(key) {
-//     if (typeof unchangable[key] === "object" && unchangable[key] !== null) {
-//         Object.freeze(unchangable[key]);
+
+// function deepFreeze(value) {
+//     if (typeof value === "object" && value !== null) {
+//         Object.freeze(value);
+//         Object.getOwnPropertyNames(value).forEach(function(key) {
+//             deepFreeze(value[key]);
+//         });
 //     }
-// })
+// }
+// deepFreeze(unchangable)
 // try {
 //     unchangable.value = 1;
-//     unchangable.nested.value = 3;
+//     // unchangable.nested.value = 3;
 // } catch (error) {
+//     console.group('error');
 //     console.log(error);
+//     console.groupEnd();
 // } finally {
-//     console.log(unchangable);
+//     console.group('finally');
+//     console.dir(JSON.stringify(unchangable));
+//     console.groupEnd();
 // }
 
 /* blocked scope funcitons */
--- src/lessons/2/index.js
+++ src/lessons/2/index.js
@@ -1,53 +1,102 @@
-/* Lesson 2, code snippents */
-
 /* New functions */
 const sentence = 'Bad Deadpool... Good Deadpool!';
 const banana = 	"\ud83c\udf4c";
+const log = console.log;
 /* strartWith */
-sentence.startsWith('bad');
-sentence.startsWith('bad', 1);
+// log(sentence.startsWith('Bad'));
+// log(sentence.startsWith('bad'));
+// log(sentence.startsWith('bad', 1));
 /* endsWith */
-sentence.endsWith('pool');
-sentence.endsWith('pool', 20);
+// log(sentence.endsWith('pool!'));
+// log(sentence.endsWith('pool', 12));
 /* includes */
-sentence.includes('Deadpool');
-sentence.includes('deadpool');
-sentence.includes('Bad', 1);
+// log(sentence.includes('Deadpool'));
+// log(sentence.includes('deadpool'));
+// log(sentence.includes('Bad', 1));
 /* repeat */
-"hey".repeat(10);
+// log("hey ".repeat(10));
 // Range Error
-"hey".repeat(-1);
-"hey".repeat(1/0) // бесонечность
+// "hey".repeat(-1);
+// "hey".repeat(1/0) // бесонечность
+// !!!! Before this, we need do some introduction to unicode
 /* codePointAt/fromCodePoint */
-const bananaCode = banana.codePointAt(0);
-String.fromCodePoint(bananaCode);
+// const bananaCode = banana.codePointAt(0);
+// log(bananaCode);
+// log(String.fromCodePoint(bananaCode));
 /* normolize */
 /* TODO */
 
 
 /* Шаблонные строки */
-`My host is ${window.location.hostname} and port is ${window.location.port}`;
+// `My host is ${window.location.hostname} and port is ${window.location.port}`;
 // escape new line
-`1\
+log(`1\
 2\
-3`
+3`)
 // escape
-`i want escape you \${}`
+log(`i want escape you \${}`)
 // line terminators always LF: \n
-`with new line
-`.search('\n')
+log(`with new line
+`.search('\n'))
 
-/* Теговые шаблоны */
-/* TODO */
+/* Теговые шаблоны (простейший пример форматирования строки)*/
+
+const formatters = {
+    date: {
+      token: ':в',
+      handler: formatDate
+    }
+  }
+  function format(strings, ...values) {
+    return strings.reduce((prev, cur, i) => {
+      const next = strings[i+1];
+      let value = values[i];
+      Object.keys(formatters).forEach(function(key) {
+        if (next && new RegExp(`^${formatters[key].token}`).test(next)) {
+          value = formatters[key].handler(value);
+          strings[i+1] = next.replace(formatters[key].token, '');
+        }
+      });
+      return `${prev}${cur}${value || ''}`
+    }, '');
+  }
+  
+  function formatDate(value) {
+    const date = new Date(value);
+    return `${('0' + (date.getMonth() + 1)).substring(-2)}\
+  .${('0' + date.getDate()).substring(-2)}\
+  .${date.getFullYear()}`
+  }
+  
+  log(format`current Date is ${'2019-03-03T14:01:33.505Z'}:d`)
 
 /* Regexp */
 /* flags */
-/s/gyi.flags
+// /s/gyi.flags
 /* sticky */
-/a/g.match('abaa');
-/a/gy.match('abaa');
+// /a/g.match('abaa');
+// /a/gy.match('abaa');
 /* TODO: more advanced and usefull case */
 /* unicode flag */
+// match code point
+log(/\u{61}/.test('a'));
+log(/\u{61}/u.test('a'));
+log(/\ud83c/.test(banana));
+log(/\ud83c/u.test(banana));
+// match for dot operator
+log(/^.$/.test(banana));
+log(/^.$/u.test(banana));
+log(/\ud83c\udf4c{2}/.test(banana.repeat(2)))
+log(/\ud83c\udf4c{2}/u.test(banana.repeat(2)))
+// match for unicode points
+log(/\u{1F34C}/u.test(banana));
+
+log(/^[ab\ud83c\udf4c]$/.test('a'));
+log(/^[ab\u0061]$/.test('a')); // same as previous
+log(/^[ab\ud83c\udf4c]$/.test(banana));
+log(/^[ab\ud83c\udf4c]$/u.test(banana));
 
 
+log(/^\S$/.test(banana)) // false
+log(/^\S$/u.test(banana)) // true
 
