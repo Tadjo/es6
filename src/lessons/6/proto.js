feee62033809c0ebee1ef2cdb7a5d05f9b77d2f9
Konstantin Bulgakov <nor.wanderer@gmail.com>
2019-04-23 18:16:45 +0300

    add another ano proto example

--- src/lessons/6/proto.js
+++ src/lessons/6/proto.js
@@ -19,6 +19,13 @@
     console.log(descendant.getMe); // undefined!!
 }
 {
+    // defineProperty перезатирает поведение свойства __proto__
+    const ancestor = { getMe: 'yep!!' };
+    const descendant = {};
+    Object.defineProperty(descendant, '__proto__', {value: ancestor}); // define Property перезатирает существующий getter/setter для proto
+    console.log(descendant.getMe); // undefined!!
+}
+{
     // при создании объекта без прототипа у него нет свойства __proto__
     const withoutProto = Object.create(null);
     console.log('__proto__' in withoutProto); // false
