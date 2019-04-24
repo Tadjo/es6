7ad6ee70b413994e5c16333896b714c3c759da8b
Konstantin Bulgakov <nor.wanderer@gmail.com>
2019-04-24 18:16:11 +0300

    weakmap example

---
+++ src/lessons/8/index.js
@@ -0,0 +1,5 @@
+// import './fromof.js';
+// import './loops.js';
+// import './map.js';
+// import './prototype-methods.js';
+import './weakmap.js';
--- src/lessons/8/weakmap.js
+++ src/lessons/8/weakmap.js
@@ -1,10 +1,23 @@
-let lessons = [...Array(10).keys()].map(id => ({ id: id+1 }));
-const meta = new WeakMap(lessons.map(lesson => [lesson, { views: 0, timing: lesson.id * 60 * 0.5 }]));
-meta.get(lessons[0]).views++;
-meta.get(lessons[1]).views++;
-console.log(meta)
-console.log(meta.get(lessons[0]));
-console.log(meta.get(lessons[1]));
-lessons = null;
-// run garbage collector from chrome devtools
-setTimeout(() => console.log(meta), 5000);
+const meta = new WeakMap();
+
+Array.from({length: 10}).forEach((value, index) => {
+    const lessonElement = document.body.appendChild(document.createElement('div'));
+    const metaInfo = { views: Math.round(Math.random() * 100), duration: index * 60 * 0.5 };
+    lessonElement.className = 'lesson';
+    meta.set(lessonElement, metaInfo);
+    lessonElement.innerHTML = `Lesson ${index}: views - ${metaInfo.views}, duration - ${metaInfo.duration/60}h`;
+});
+
+const deleteBtn = document.body.appendChild(document.createElement('button'));
+const consoleBtn = document.body.appendChild(document.createElement('button'));
+deleteBtn.innerHTML = 'Delete Random';
+consoleBtn.innerHTML = 'Check';
+
+deleteBtn.addEventListener('click', () => {
+    const index = Math.round(Math.random() * 9);
+    document.querySelectorAll('.lesson')[index].remove();
+});
+
+consoleBtn.addEventListener('click', () => {
+    console.dir(meta);
+});
