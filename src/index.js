d2862b71b5b752e8dbc963d267fd793298cb3a2f
Konstantin Bulgakov <nor.wanderer@gmail.com>
2019-05-21 17:20:32 +0300

    module examples: fixes

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
-const lib = require('imports-loader?define=>false!./lessons/10/umd');
+// const lib = require('./lessons/10/common');
+const lib = require('./lessons/10/umd');
+// const lib = require('imports-loader?define=>false!./lessons/10/umd');
 console.log(lib);
--- src/lessons/10/IIFE.js
+++ src/lessons/10/IIFE.js
@@ -5,3 +5,5 @@
 
     createWidget();
 })();
+
+console.log(window.createWidget);
